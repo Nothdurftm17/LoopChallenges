4c388253aed3564f6031be4401ff6b06ba823f4e
Matthew Nothdurft <nothdurftm17@gmail.com>
2022-01-29 18:47:57 -0500

    Loop Challenges

---
+++ index.js
@@ -0,0 +1,30 @@
+for(var i=1;i<20;i++){
+    if(i%2 != 0)
+        console.log(i)
+}
+
+
+for(var i = 100; i>0;i--){
+    if(i%3 == 0)
+    console.log(i)
+}
+
+
+var arr=[4,2.5,1,-0.5,-2,-3.5]
+for(var i = 0; i<arr.length;i++){
+    console.log(arr[i])
+}
+
+
+var sum = 0
+for(var i = 1; i<101;i++){
+    sum += i
+}
+console.log(sum)
+
+
+var product = 1
+for(var i =1; i<13;i++){
+    product= product * i
+}
+console.log(product)
